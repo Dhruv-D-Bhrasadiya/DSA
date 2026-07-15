3a2640dd2365593b903640719005a2c792f83162
Dhruv D. Bhrasadiya <dhruvbhrasadiya@gmail.com>
2026-07-15 16:32:51 +0530

    add a easy math problem

---
+++ math/GCD_of_odd_and_even_sums.java
@@ -0,0 +1,34 @@
+package math;
+
+/* 
+Approach: Math
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int even = (n ) * (n - 1);
+        int odd = (int) Math.pow((n), 2);
+
+        while(odd != 0){
+            int rem = even % odd;
+            even = odd;
+            odd = rem;
+        }
+
+        return even;
+    }
+}
+*/
+    
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
+
+public class GCD_of_odd_and_even_sums {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 5; // Example input
+        int result = sol.gcdOfOddEvenSums(n);
+        System.out.println("GCD of odd and even sums for n = " + n + " is: " + result);
+    }
+}
