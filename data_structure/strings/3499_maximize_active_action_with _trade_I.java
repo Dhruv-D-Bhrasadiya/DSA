e4ccfd522360ee84215a2fdd9e347389fb912a62
Dhruv D. Bhrasadiya <dhruvbhrasadiya@gmail.com>
2026-07-21 17:19:50 +0530

    solved string

---
+++ data_structure/strings/3499_maximize_active_action_with _trade_I.java
@@ -0,0 +1,48 @@
+package data_structure.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+
+        List<Integer> list = new ArrayList<>();
+        int len = 0;
+
+        for (char c : s.toCharArray())
+            if (c == '1') {
+                if (len != 0) {
+                    list.add(len);
+                    len = 0;
+                }
+            } else
+                len++;
+
+        if (len != 0)
+            list.add(len);
+
+        int count = 0;
+        for (char c : s.toCharArray())
+            if (c == '1')
+                count++;
+
+        if (list.size() <= 1)
+            return count;
+
+        int max = 0;
+        for (int i = 0; i < list.size() - 1; i++)
+            max = Math.max(max, list.get(i) + list.get(i + 1));
+
+        return count + max;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "1000100";
+        int result = solution.maxActiveSectionsAfterTrade(s);
+        
+        System.out.println("Maximized Active Action: " + result);
+    }
+}
