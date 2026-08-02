a83f98a04a2b8f581018bbfdb609c4b4359e3a25
Dhruv D. Bhrasadiya <dhruvbhrasadiya@gmail.com>
2026-08-02 23:10:11 +0530

    dp game theory

---
+++ algorithms/dp/877_stone_game.java
@@ -0,0 +1,17 @@
+package algorithms.dp;
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int[] piles = {5, 3, 4, 5};
+        boolean result = solution.stoneGame(piles);
+        System.out.println(result); // Output: true
+    }
+}
