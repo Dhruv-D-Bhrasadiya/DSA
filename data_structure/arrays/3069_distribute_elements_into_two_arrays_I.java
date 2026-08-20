6ff98bcd1525fe5b9a580a64cf183f4674bc4e9a
Dhruv D. Bhrasadiya <dhruvbhrasadiya@gmail.com>
2026-08-21 00:05:44 +0530

    add optimized solution

--- data_structure/arrays/3069_distribute_elements_into_two_arrays_I.java
+++ data_structure/arrays/3069_distribute_elements_into_two_arrays_I.java
@@ -1,6 +1,6 @@
 package data_structure.arrays;
 
-
+/*
 import java.util.List;
 import java.util.ArrayList;
 
@@ -39,6 +39,49 @@ class Solution {
         for(int i = l1.size(); i < n; i++){
             ans[i] = l2.get(p++);
         }
+        return ans;
+    }
+}
+Optimal solution given below:
+ */
+
+class Solution {
+    public int[] resultArray(int[] nums) {
+        int n = nums.length;
+        if(n <= 2){
+            return nums;
+        }
+
+        int a = nums[0];
+        int b = nums[1];
+        int p1 = 1;
+
+        nums[1] *= -1;
+
+        for(int i = 2; i < n; i++){
+            if(a > b){
+                a = nums[i];
+                p1++;
+            }
+            else{
+                b = nums[i];
+                nums[i] = -1 * nums[i];
+            }
+        }
+        
+        int[] ans = new int[n];
+        int idx1 = 0; 
+        int idx2 = p1;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] < 0){
+                ans[idx2++] = nums[i] * -1;
+            }
+            else{
+                ans[idx1++] = nums[i];
+            }
+        }
+
         return ans;
     }
 }
