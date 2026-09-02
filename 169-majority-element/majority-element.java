df0837cc0920c47c86df216918aefba8ac2934bc
Dhruv D. Bhrasadiya <83451710+Dhruv-D-Bhrasadiya@users.noreply.github.com>
2026-09-02 14:36:11 +0530

    Time: 2 ms (70.75%) | Memory: 55.4 MB (69.04%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,11 +1,31 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> hm = new HashMap<>();
-
-        for(int i = 0; i < nums.length; i++){
-            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+        int cand = findCandidate(nums);
+        int count = 0;
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(cand == nums[i]){
+                count++;
+            }
         }
+        return (n / 2) < count ? cand : -1;
+    }
 
-        return Collections.max(hm.entrySet(), Map.Entry.comparingByValue()).getKey();
+    public int findCandidate(int[] nums){
+        int n = nums[0];
+        int count = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(n == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+                if(count == 0){
+                    n = nums[i];
+                    count++;
+                }
+            }
+        }
+        return n;
     }
 }
