c154f1be31bc6c80ed16c1c691c968efe129a395
Dhruv D. Bhrasadiya <83451710+Dhruv-D-Bhrasadiya@users.noreply.github.com>
2026-09-06 15:04:10 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (61.24%) - LeetSync

--- 4411-count-rotations-with-exactly-k-equal-adjacent-pairs/count-rotations-with-exactly-k-equal-adjacent-pairs.java
+++ 4411-count-rotations-with-exactly-k-equal-adjacent-pairs/count-rotations-with-exactly-k-equal-adjacent-pairs.java
@@ -1,24 +1,22 @@
 class Solution {
     public int countRotations(String s, int k) {
-        int count = 0;
         int n = s.length();
+        int equal = 0;
 
-        String d = s + s;
-
-        for(int i = 0; i < n; i++){
-            String rotate = d.substring(i, i + n);
-
-            int score = 0;
-            for(int j = 0; j < n - 1; j++){
-                if(rotate.charAt(j) == rotate.charAt(j + 1)){
-                    score++;
-                }
-            }
-
-            if(score == k){
-                count++;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == s.charAt((i + 1) % n)) {
+                equal++;
             }
         }
-        return count;
+
+        if (k == equal - 1) {
+            return equal;
+        }
+
+        if (k == equal) {
+            return n - equal;
+        }
+
+        return 0;
     }
 }
